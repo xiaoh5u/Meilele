a4938c5401adfb2ad2d8c7b732fea5accc92b275
xiaoh5u <xiaoh5u@foxmail.com>
2019-08-26 13:33:07 +0800

    备份

--- dist/js/settle_accounts.js
+++ dist/js/settle_accounts.js
@@ -5,10 +5,118 @@ $.ajax({
 }).then(({
     list,
 }) => {
-   for(var i=0;i<list.length;i++){
-    var name = list[i].name
-    var html =`
-    <option value="">${name}</option>`
-    $('#province').append(html)
-   }
+    list.forEach(({
+        name,
+        city_id
+    }) => {
+        $html = `
+        <option value="${city_id}">${name}</option>`
+        $('#province').append($html)
+    })
+
+    $('.value').on('change', '#province', function () {
+        $('#city').css('display', 'inline-block')
+        $('#city').html(' <option value="">请选择城市</option>')
+        $('#area').html(' <option value="">请选择地区</option>')
+        $('.city').html('')
+        $('.area').html('')
+        $prov = $(this).val();
+        $obj = list.filter(item => item.city_id === $prov)[0]
+        $('.province').html($obj.name)
+        $obj.list.forEach(({
+            name,
+            city_id
+        }) => {
+            $html = `
+        <option value="${city_id}">${name}</option>`
+            $('#city').append($html)
+
+        })
+        // 二级城市对象
+        $second_city = $obj.list
+    })
+    //城市发生改变
+    $('.value').on('change', '#city', function () {
+        $('#area').css('display', 'inline-block')
+        $('#area').html(' <option value="">请选择地区</option>')
+        $('.area').html('')
+        $prov = $(this).val();
+        $obj = $second_city.filter(item => item.city_id === $prov)[0]
+        $('.city').html($obj.name)
+        if ($obj.list) {
+            $obj.list.forEach(({
+                name,
+                city_id
+            }) => {
+                $html = `
+            <option value="${city_id}">${name}</option>`
+                $('#area').append($html)
+            })
+        } else {
+            $('#area').css('display', 'none')
+        }
+        $area = $obj.list
+
+    })
+    $('.value').on('change', '#area', function () {
+        $prov = $(this).val();
+        $obj = $area.filter(item => item.city_id === $prov)[0]
+        $('.area').html($obj.name)
+
+    })
+})
+
+
+//地址栏
+$('.close').on('click', function () {
+    $('.info-input').css('display', 'none')
+})
+
+$('.addAddress').on('click', function () {
+    $('.info-input').css('display', 'block')
+})
+
+$('.right-add').on('click', function () {
+    $('.info-input').css('display', 'block')
+})
+
+
+$('#addressSubmit').on('click', function () {
+    $name = $('#name').val()
+    if ($('.area').html()) {
+        $address = $('.province').html() + $('.city').html() + $('.area').html() + $('#detail').val();
+    } else {
+        $address = $('.province').html() + $('.city').html() + $('#detail').val();
+    }
+    $tel = $('#tel').val()
+    $username = $('.username').html()
+    if (!$name || !$address || !$tel) {
+        return
+    }
+    $.ajax({
+        url: 'http://localhost/php/project_php/user_address.php',
+        type: 'post',
+        dataType: 'json',
+        data: {
+            'username': $username,
+            'name': $name,
+            'address': $address,
+            'tel': $tel
+        }
+    }).then(({
+        msg,
+        code,
+        data,
+    }) => {
+        if (code) {
+            alert(msg)
+        } else {
+
+        }
+
+
+
+    })
+
+
 })
